b90af96d3ffca5a7e8a67aaf7a9c4bde279f5167
gautm <gaut.montagne@gmail.com>
2023-09-06 11:53:36 +0200

    Add: Sidebar component + tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin');
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
