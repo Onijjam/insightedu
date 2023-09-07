81305e4cd1df59f67625cf1d3a437d8e46ceb6d3
gautm <gaut.montagne@gmail.com>
2023-09-07 08:56:23 +0200

    Add: système de routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
 import {Sidebar} from "./components/Sidebar.jsx";
 import {Login} from "./pages/Login.jsx";
-import {ErrorPage} from "./pages/ErrorPage.jsx";
+import { Outlet } from "react-router-dom";
 import {useEffect, useState} from "react";
-import {ContactSupport} from "./pages/ContactSupport.jsx";
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -24,8 +23,7 @@ function App() {
             {isLoggedIn ? (
                 <Sidebar>
                     <div className={"bg-gray-900/10 w-full h-full rounded-md lg:p-5"}>
-                        {/*<ErrorPage/>*/}
-                        <ContactSupport />
+                        <Outlet />
                     </div>
                 </Sidebar>
             ) : (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/root";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
