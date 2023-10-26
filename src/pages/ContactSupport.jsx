d502a9bc768efa2a761500ecdcb7054af55fde14
gautm <gaut.montagne@gmail.com>
2023-10-26 12:12:42 +0200

    Add: Profile page + Navbar notifications

--- src/pages/ContactSupport.jsx
+++ src/pages/ContactSupport.jsx
@@ -1,6 +1,6 @@
 export function ContactSupport() {
     return (
-        <form className={"grid content-center h-[92vh] lg:h-[80vh] place-items-center bg-white rounded-md lg:shadow-xl lg:px-8"}>
+        <form className={"grid content-center py-8 place-items-center bg-white rounded-md lg:shadow-xl lg:px-8"}>
             <div className="space-y-12">
                 <div className="border-b border-gray-900/10 pb-12">
                     <h2 className="text-2xl font-semibold leading-7 text-gray-900">Formulaire de contact</h2>
@@ -17,7 +17,7 @@ export function ContactSupport() {
                                     name="first-name"
                                     id="first-name"
                                     autoComplete="given-name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -32,7 +32,7 @@ export function ContactSupport() {
                                     name="last-name"
                                     id="last-name"
                                     autoComplete="family-name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -47,7 +47,7 @@ export function ContactSupport() {
                                     name="email"
                                     type="email"
                                     autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -61,7 +61,7 @@ export function ContactSupport() {
                                     rows={4}
                                     name="comment"
                                     id="comment"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-500 sm:text-sm sm:leading-6"
                                     defaultValue={''}
                                 />
                             </div>
@@ -76,7 +76,7 @@ export function ContactSupport() {
                 </button>
                 <button
                     type="submit"
-                    className="rounded-md bg-cyan-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md bg-cyan-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                     Envoyer
                 </button>
